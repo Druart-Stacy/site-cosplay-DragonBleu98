72196d34637b03f8e4551a9f185031217139cc48
Stacy Druart <stacy@StacyDruartMSI>
2025-01-03 15:11:53 +0100

    maj

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        'custom-blue': '#6A96E8',
-      },
-      backgroundImage: {
-        'custom-bg': "url('/images/background.jpg')",
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 }
